cccb9880efd0a119595f79a01db2dad0d0019518
Sharad Solanki <sharad.solanki@wavemaker.com>
2017-04-19 06:47:23 +0000

    updated the report pages New files added - [reportsLeftNav,StatusChangeReport] Files modified - [Main,Report,PersonHistory,app.variables.json,pages-config.json]

--- src/main/webapp/pages/StatusChangeReport/StatusChangeReport.js
+++ src/main/webapp/pages/StatusChangeReport/StatusChangeReport.js
@@ -1,4 +1,4 @@
-Application.$controller("ReportPageController", ["$scope", function ($scope) {
+Application.$controller("StatusChangeReportPageController", ["$scope", function ($scope) {
     "use strict";
 
     /* perform any action on widgets/variables within this block */
@@ -15,11 +15,3 @@ Application.$controller("ReportPageController", ["$scope", function ($scope) {
     };
 
 }]);
-
-
-Application.$controller("grid1Controller", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
-]);
