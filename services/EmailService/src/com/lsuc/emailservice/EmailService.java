ab09213f9cacd25bc8427a1f3bb05e8ec0c2947e
harish harishvanama <harish.vanama+73@wavemaker.com>
2017-04-24 09:54:57 +0000

    Added Junit testcase at src main com lsuc tests New files added - [tests] Files modified - [personProfile,LSUC,EmailService,pom.xml]

--- services/EmailService/src/com/lsuc/emailservice/EmailService.java
+++ services/EmailService/src/com/lsuc/emailservice/EmailService.java
@@ -3,18 +3,6 @@
  with the terms of the source code license agreement you entered into with wavemaker-com*/
 package com.lsuc.emailservice;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-
-import com.wavemaker.runtime.security.SecurityService;
-import com.wavemaker.runtime.service.annotations.ExposeToClient;
-import com.wavemaker.runtime.service.annotations.HideFromClient;
-
 import java.util.Properties;
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -22,6 +10,13 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.wavemaker.runtime.security.SecurityService;
+import com.wavemaker.runtime.service.annotations.ExposeToClient;
 
 //import com.lsuc.emailservice.model.*;
 
@@ -39,9 +34,9 @@ import javax.mail.internet.MimeMessage;
 public class EmailService {
 
     private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
-    private static final String NO_REPLY_MAIL_ID ="pramati.wave@gmail.com";
-    private static final String NO_REPLY_MAIL_PASSWORD ="Pr@m@t!123";
-    private static final String SUPERVISOR_MAIL_ID ="tribhuvan.durgam@wavemaker.com";
+    private static final String NO_REPLY_MAIL_ID = "pramati.wave@gmail.com";
+    private static final String NO_REPLY_MAIL_PASSWORD = "Pr@m@t!123";
+    private static final String SUPERVISOR_MAIL_ID = "tribhuvan.durgam@wavemaker.com";
     //private static final String FORM_URL = "http://e12561a71473b.cloud.wavemakeronline.com/CivicXpress/#/Forms?
 
     @Autowired
@@ -50,15 +45,17 @@ public class EmailService {
     /**
      * This is sample java operation that accepts an input from the caller and responds with "Hello".
      *
-     * SecurityService that is Autowired will provide access to the security context of the caller. It has methods like isAuthenticated(),
+     * SecurityService that is Autowired will provide access to the security context of the caller. It has methods like
+     * isAuthenticated(),
      * getUserName() and getUserId() etc which returns the information based on the caller context.
      *
      * Methods in this class can declare HttpServletRequest, HttpServletResponse as input parameters to access the
-     * caller's request/response objects respectively. These parameters will be injected when request is made (during API invocation).
+     * caller's request/response objects respectively. These parameters will be injected when request is made (during
+     * API invocation).
      */
     // public String sampleJavaOperation(String name, HttpServletRequest request) {
     //     logger.debug("Starting sample operation with request url " + request.getRequestURL().toString());
-        
+
     //     String result = null;
     //     if (securityService.isAuthenticated()) {
     //         result = "Hello " + name + ", You are logged in as "+  securityService.getLoggedInUser().getUserName();
@@ -68,8 +65,7 @@ public class EmailService {
     //     logger.debug("Returning {}", result);
     //     return result;
     // }
-    
-    public String sendEmail(String licenseeNumber,String licenseeName, String personPk) {
+    public String sendEmail(String licenseeNumber, String licenseeName, String personPk) {
         try {
             // Use javamail api, set parameters from registration.properties file
             // set the session properties
@@ -80,25 +76,25 @@ public class EmailService {
             props.put("mail.smtp.auth", "true");
             props.put("mail.smtp.starttls.required", "true");
             Session session = Session.getDefaultInstance(props, null);
-            
-            String emailSubject = "Licensee Status Change for "+ licenseeNumber;
+
+            String emailSubject = "Licensee Status Change for " + licenseeNumber;
             String emailMessage = "Hi <br><br>";
-            String approveUrl = "https://www.wavemakeronline.com/run-53t562sphl/LSUC/#/Approvals?id="+personPk;
-            logger.info("*********************Approve URL****************"+approveUrl);
-            
-            emailMessage = emailMessage+"The Licensee Status for <b>" + licenseeName+"</b>";
-            emailMessage = emailMessage+" with Licensee Number: <b>"+licenseeNumber+"</b> has been changed. Please review and provide your approval.<br><br>";
-            emailMessage = emailMessage+"Click <a href = '"+approveUrl+"'>here</a> to review changes.<br><br>";
-             emailMessage = emailMessage+"Regards<br>"+securityService.getLoggedInUser().getUserName()+"<br><br>";
-             emailMessage = emailMessage+"Note: This is a system generated email. Kindly do not reply.";
-            
+            String approveUrl = "https://www.wavemakeronline.com/run-53t562sphl/LSUC/#/Approvals?id=" + personPk;
+            logger.info("*********************Approve URL****************" + approveUrl);
+
+            emailMessage = emailMessage + "The Licensee Status for <b>" + licenseeName + "</b>";
+            emailMessage = emailMessage + " with Licensee Number: <b>" + licenseeNumber + "</b> has been changed. Please review and provide your approval.<br><br>";
+            emailMessage = emailMessage + "Click <a href = '" + approveUrl + "'>here</a> to review changes.<br><br>";
+            emailMessage = emailMessage + "Regards<br>" + securityService.getLoggedInUser().getUserName() + "<br><br>";
+            emailMessage = emailMessage + "Note: This is a system generated email. Kindly do not reply.";
+
             // Create email message
             MimeMessage message = new MimeMessage(session);
             message.setFrom(new InternetAddress(NO_REPLY_MAIL_ID));
             String[] recipientList = SUPERVISOR_MAIL_ID.split(",");
             InternetAddress[] recipientAddress = new InternetAddress[recipientList.length];
             int counter = 0;
-            for (String recipient: recipientList) {
+            for (String recipient : recipientList) {
                 recipientAddress[counter] = new InternetAddress(recipient.trim());
                 counter++;
             }
@@ -116,6 +112,10 @@ public class EmailService {
 
         } catch (MessagingException e) {
             return "Error in method sendEmailNotification: " + e;
-        }}
+        }
+    }
 
+    public String helloWorld(String user) {
+        return "Hello " + user;
+    }
 }
---
+++ src/test/java/com/lsuc/tests/JunitTestSuite.java
@@ -0,0 +1,15 @@
+package com.lsuc.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by harishv on 4/21/2017.
+ */
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        TestJavaService.class
+})
+public class JunitTestSuite {
+}
