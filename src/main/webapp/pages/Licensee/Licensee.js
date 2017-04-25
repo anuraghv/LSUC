2498523ffbf15a59a1b3b57cd933994fdb19d733
Abhilash Sandi <abhilash.sandi@wavemaker.com>
2017-04-25 12:36:11 +0000

    Updated the add new licensee form Files modified - [Licensee,License]

--- src/main/webapp/pages/Licensee/Licensee.js
+++ src/main/webapp/pages/Licensee/Licensee.js
@@ -20,8 +20,26 @@ Application.$controller("LicenseePageController", ["$scope", "$timeout", functio
         if ($scope.Widgets.licenseeclasspracticegroupsForm.dataoutput.effectiveToDate === undefined) {
             $scope.Widgets.licenseeclasspracticegroupsForm.dataoutput.effectiveToDate = "2999-12-31";
         }
-        /*Add data to licenseeclasspracticegroups from the inner liveform data*/
-        $data['licenseeclasspracticegroups'] = [$scope.Widgets.licenseeclasspracticegroupsForm.dataoutput];
+    };
+
+    /*On licensee form success, Add the licensee status data to the approval data*/
+    $scope.addLicenseeSuccess = function($event, $operation, $data) {
+        var dataOutput = $scope.Widgets.licenseeclasspracticegroupsForm.dataoutput;
+        /* If effective date is not selected, set the default value*/
+        if (dataOutput.effectiveToDate == undefined) {
+            dataOutput.effectiveToDate = '9999-12-31';
+        }
+        /* Set the inputs for LSUC_ApprovalData variable*/
+        $scope.Variables.LSUC_ApprovalData.setInput({
+            "licenseeFk": $data.pk,
+            "newIsPrimary": dataOutput.isPrimary,
+            "newEffectiveFromDate": dataOutput.effectiveFromDate,
+            "newEffectiveToDate": dataOutput.effectiveToDate,
+            "newClassPracticeGroupFk": dataOutput.classpraticegroup.pk,
+            "status": "Created"
+        });
+        /* Insert data in LSUC_ApprovalData*/
+        $scope.Variables.LSUC_ApprovalData.insertRecord();
     };
 
     /*This method is called before the variable call is made*/
